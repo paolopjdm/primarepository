a9b0faa9de9eece8004c57306c623a668b388f6c
Utente <Utente@LENOVO-AMD>
2018-10-26 17:47:57 +0200

    update

--- primarepository/src/primarepository/Main.java
+++ primarepository/src/primarepository/Main.java
@@ -3,10 +3,10 @@ package primarepository;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("FORZA LAZIO");
+		System.out.println("FORZA ROMA");
         for(int i=0;i<10;i++) {
         	System.out.println("luca non te arrabbià");
-        }
+        
 	}
-
+	}        
 }
