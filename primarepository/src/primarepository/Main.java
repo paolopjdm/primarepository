db52d25d7cd1edea91431362b0d69183e980c8bf
Admin <Admin@DESKTOP-EF15FFA>
2018-10-26 17:23:41 +0200

    message for Luca

--- primarepository/src/primarepository/Main.java
+++ primarepository/src/primarepository/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("FORZA LAZIO");
-
+        for(int i=0;i<10;i++) {
+        	System.out.println("luca non te arrabbià");
+        }
 	}
 
 }
